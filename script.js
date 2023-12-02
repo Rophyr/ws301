01d941f73ed7c82786cabe83cca4cb6d2a2db2ce
Okesse Smith <okesse99@gmail.com>
2023-12-02 20:41:31 +0100

    homepage ajustement

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     document.addEventListener('mousemove', (e) => {
       if (isDragging) {
-        const x = e.clientX - offsetX - 410;
+        const x = e.clientX - offsetX;
         const y = e.clientY - offsetY;
         element.style.left = `${x}px`;
         element.style.top = `${y}px`;
